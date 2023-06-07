7ebe9557ec5fd205399f42b05f5432e37f0cc069
YUNYEONG-DEV <121932063+YUNYEONG-DEV@users.noreply.github.com>
2023-06-07 10:43:48 +0900

    MainScreen 폰트 크기 수정

--- app/src/main/java/kr/co/bbmc/paycastdid/presentation/main/MainScreen.kt
+++ app/src/main/java/kr/co/bbmc/paycastdid/presentation/main/MainScreen.kt
@@ -5,30 +5,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.asLiveData
 import com.orhanobut.logger.Logger
 import kotlinx.coroutines.FlowPreview
 import kr.co.bbmc.paycastdid.R
-import kr.co.bbmc.paycastdid.network.model.CookingData
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -36,17 +29,23 @@ import kr.co.bbmc.paycastdid.network.model.CookingData
 @Composable
 fun MainScreen(vm: MainViewModel) {
     val didItems = vm.didInfo.collectAsState().value?.groupBy { it.orderNumber }
-    val dpInfo = vm.dpInfo.observeAsState().value
-//    val didItemList: List<CookingData> = didItems?.flatMap { it.value } ?: emptyList()
+
+
+    Logger.e("윤영 didItems : ${didItems.toString()}")
 
     didItems?.keys?.forEach { key ->
         val values = didItems[key]
-        Logger.e("Group by Test == Key : $key, Values: $values")
+        Logger.e(" 윤영 Group by Test == Key : $key, Values: $values")
     }
 
     Column {
-        Text(
-            text = vm.dpInfo.value.toString()
+        Image(
+            painter = painterResource(id = R.drawable.sample_rabbit),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.6f),
+            contentScale = ContentScale.Crop
         )
         Row(
             modifier = Modifier
@@ -74,7 +73,7 @@ fun MainScreen(vm: MainViewModel) {
                             ) {
                                 Text(
                                     text = "$key",
-                                    fontSize = 70.sp,
+                                    fontSize = 120.sp,
                                     color = Color.Black,
                                     fontWeight = FontWeight.Bold,
                                     textAlign = TextAlign.Center,
@@ -84,14 +83,14 @@ fun MainScreen(vm: MainViewModel) {
                                     Row {
                                         Text(
                                             text = "${it.menuName}",
-                                            fontSize = 20.sp,
+                                            fontSize = 45.sp,
                                             color = if(it.cookingState == "N") Color.Black else Color.Red,
                                             modifier = Modifier.basicMarquee()
                                         )
                                         Spacer(modifier = Modifier.width(16.dp))
                                         Text(
                                             text = "${it.count}",
-                                            fontSize = 20.sp,
+                                            fontSize = 45.sp,
                                             color = Color.Gray,
                                         )
                                     }
@@ -101,81 +100,30 @@ fun MainScreen(vm: MainViewModel) {
                     }
                 }
             }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.sample_cocatu),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.7f),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "대기 0",
+                    fontSize = 50.sp,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Red
+                )
+            }
         }
     }
-
-//    Column {
-////        Image(
-////            painter = painterResource(id = R.drawable.sample_cocatu),
-////            contentDescription = null,
-////            modifier = Modifier
-////                .fillMaxWidth()
-////                .fillMaxHeight(0.6f),
-////            contentScale = ContentScale.Crop
-////        )
-//        Row(
-//            modifier = Modifier
-//                .background(Color.White)
-//        ) {
-//            LazyRow(
-//                modifier = Modifier
-//                    .fillMaxHeight()
-//                    .fillMaxWidth(0.7f)
-//                    .padding(10.dp)
-//            ) {
-//                items(didItemList) {
-//                    Box(
-//                        modifier = Modifier
-//                            .padding(24.dp)
-//                            .height(380.dp)
-//                            .width(380.dp)
-//                            .background(Color.LightGray, shape = RoundedCornerShape(48.dp))
-//                    ) {
-//                        Column(
-//                            modifier = Modifier
-//                                .padding(24.dp)
-//                                .fillMaxSize()
-//                        ) {
-//                            Text(
-//                                text = it.orderNumber.toString(),
-////                                fontSize = 120.sp,
-//                                fontSize = 70.sp,
-//                                color = Color.Black,
-//                                fontWeight = FontWeight.Bold,
-//                                textAlign = TextAlign.Center,
-//                                modifier = Modifier.fillMaxWidth()
-//                            )
-////                            Row(
-////                                modifier = Modifier.basicMarquee()
-////                            ) {
-////                                repeat(didItemList.size) {
-////                                    LazyRow(
-////                                        modifier = Modifier
-////                                    ) {
-////                                        items(didItemList) {
-////                                            Text(
-////                                                text = it.menuName.toString(),
-////                                                fontSize = 50.sp,
-////                                                color = if(it.cookingState == "N") Color.Black else Color.Red,
-////                                                maxLines = 1,
-////                                                modifier = Modifier.wrapContentSize(unbounded = true)
-////                                            )
-////                                            Spacer(modifier = Modifier.width(8.dp))
-////                                            Text(
-////                                                text = it.count.toString(),
-////                                                fontSize = 50.sp,
-////                                                color = if(it.cookingState == "N") Color.Black else Color.Red,
-////                                                maxLines = 1,
-////                                                modifier = Modifier.wrapContentSize(unbounded = true)
-////                                            )
-////                                        }
-////                                    }
-////                                }
-////                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
